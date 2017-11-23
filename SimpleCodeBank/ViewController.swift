774baa023bb46e4a137e6a13058c41966e915d90
your name <your@email.com>
2017-11-23 13:51:26 +0800

    First version.

---
+++ SimpleCodeBank/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimpleCodeBank
+//
+//  Created by John Stringer on 23/11/2017.
+//  Copyright © 2017 John Stringer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
